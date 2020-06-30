1e61b4410226e92e55ba4cf1f59f4187b16b0392
Egor <umepab@yandex.ru>
2020-06-30 18:40:52 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_2.png'/>
+    </header>
+
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://fullhdwall.com/wp-content/uploads/2018/06/Free-Summer-Image.jpeg'/>
+        </div>
+        <div>
+            ava+descr
+        </div>
+        <div>
+            posts
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            post 1
+        </div>
+        <div>
+            post 2
+        </div>
+    </div>
+}
+
+export default Profile;
